f61a5c7bd692ecaac48ba7347e09ca5c14aca3b3
Carlos <568187512@qq.com>
2023-08-23 10:59:43 +0800

    chore: update dependences
    Signed-off-by: Carlos <568187512@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Carlos
  * @Date: 2023-01-26 00:52:36
- * @LastEditTime: 2023-05-02 18:43:59
+ * @LastEditTime: 2023-08-23 10:53:18
  * @FilePath: /vue3-cms/vite.config.ts
  * @Description: null
  */
@@ -28,8 +28,8 @@ export default defineConfig({
         // rewrite: path => path.replace(/^\/music-api/, '')
       },
       '/nest-api': {
-        // target: 'http://106.55.147.116',
-        target: 'http://localhost:3001',
+        target: 'http://106.55.147.116',
+        // target: 'http://localhost:3001',
         changeOrigin: true
         // rewrite: path => path.replace(/^\/music-api/, '')
       }
